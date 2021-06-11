b44b3288ec3a9074cc9551861c6b33dde42e927b
Matic Drobez <matic.drobez@gmail.com>
2021-06-11 22:05:35 +0200

    First init commit in project discord-bot.

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+use std::env;
+
+const HELP_MESSAGE: &str = "Hello there, Human!\
+\
+The our main channel is #852884942089748532";
+
+const HELP_COMMAND: &str = "!help";
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == HELP_COMMAND {
+            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected.", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let mut client = Client::new(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client.");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
